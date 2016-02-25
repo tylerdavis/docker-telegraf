cd670bb8e10604d31be6851cacf2b48fd1499f09
Tyler Davis <tyler@tmd.io>
2016-02-25 15:28:21 -0800

    asd

--- run.sh
+++ run.sh
@@ -7,10 +7,6 @@ if [ "${INFLUXDB_HOST}" == "**None**" ]; then
   unset INFLUXDB_HOST
 fi
 
-if [ -n "${INFLUXDB_HOST}" ]; then
-  sed -i -u "s#urls = .*#urls = [\"http://${INFLUXDB_HOST}:8086\"]#g" ${CONFIG_FILE}
-fi
-
 echo "=> Starting Telegraf ..."
 
-exec /opt/telegraf/telegraf -config ${CONFIG_FILE}
+exec telegraf -config ${CONFIG_FILE}
